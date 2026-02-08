dc4705f7a33c0372df0ffd59ac09f98eefa7dfb8
Maxence CHOSIEL <maxencechoisel@gmail.com>
2026-02-08 12:08:05 +0100

    feat: Mettre à jour la description de l'API et supprimer les informations de licence dans OpenApiConfig

--- src/main/java/com/imt/api_invocations/config/OpenApiConfig.java
+++ src/main/java/com/imt/api_invocations/config/OpenApiConfig.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
@@ -23,17 +22,13 @@ public class OpenApiConfig {
         return new OpenAPI()
                 .info(new Info()
                         .title("API Invocations - Gatch Game")
-                        .description("API de gestion des invocations, monstres et compétences pour le jeu Gatch. " +
+                        .description("API de gestion des invocations de monstres et de leurs compétences pour le jeu Gatcha. " +
                                 "Cette API permet de créer, consulter, modifier et supprimer des monstres et leurs compétences, " +
                                 "ainsi que d'effectuer des invocations aléatoires basées sur des taux de drop configurables.")
                         .version("1.0.0")
                         .contact(new Contact()
                                 .name("Équipe IMT Gatch")
-                                .email("contact@gatch-game.imt")
-                                .url("https://github.com/imt/gatch-api"))
-                        .license(new License()
-                                .name("Apache 2.0")
-                                .url("https://www.apache.org/licenses/LICENSE-2.0.html")))
+                                .url("https://github.com/Maxe2005/GatchaApi")))
                 .servers(List.of(
                         new Server()
                                 .url("http://localhost:8085")
