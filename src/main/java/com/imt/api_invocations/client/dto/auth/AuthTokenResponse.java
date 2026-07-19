2fa874afbc9cfba5679047854d0a81a173520fa1
Maxence CHOISEL <maxencechoisel@gmail.com>
2026-07-19 17:04:48 +0200

    feat: Ajoute la gestion des propriétés JSON ignorées dans AuthTokenResponse

--- src/main/java/com/imt/api_invocations/client/dto/auth/AuthTokenResponse.java
+++ src/main/java/com/imt/api_invocations/client/dto/auth/AuthTokenResponse.java
@@ -1,5 +1,6 @@
 package com.imt.api_invocations.client.dto.auth;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,6 +8,8 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class AuthTokenResponse {
   private String username;
+  private String role;
 }
